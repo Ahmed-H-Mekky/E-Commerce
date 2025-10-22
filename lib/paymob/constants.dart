fb3a5b33fe55628eb2e11d07ed98698d0f20e1e9
ahmedhusssein <engineer.ahmed01010@gmail.com>
2025-10-22 18:10:34 +0300

    3

--- lib/paymob/constants.dart
+++ lib/paymob/constants.dart
@@ -4,4 +4,6 @@ class KConstants {
   static String get apiKey => dotenv.env['API_KEY'] ?? '';
   static String get cardPaymentMethodIntegrationId =>
       dotenv.env['CARD_METHOD_ID'] ?? '';
+  static String get secretKey => dotenv.env['Secret_Key'] ?? '';
+  static String get publishableKey => dotenv.env['Publishable_Key'] ?? '';
 }
